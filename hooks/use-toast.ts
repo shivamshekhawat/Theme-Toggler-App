1c13c0f4a38486693f91d7f25f09619a60c98db6
Shivam Shekhawat <admin@admins-MBP.lan>
2025-11-01 07:51:38 +0530

    Fixes bugs

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 
 export function useToast() {
   return {
-    toast: useCallback(({ title, description }) => {
+    toast: useCallback(({ title, description }: { title: string; description?: string }) => {
       alert(`${title}\n${description ?? ''}`);
     }, []),
   };
